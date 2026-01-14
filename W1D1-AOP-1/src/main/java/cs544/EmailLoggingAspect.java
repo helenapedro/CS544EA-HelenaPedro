02a9e1b8cca5d7f584c64fe65b54b7efa709ffe3
Helena Pedro <mbeua94@gmail.com>
2026-01-14 10:51:45 -0600

    Complete Task 4 — Part (b): Log Method Arguments

--- W1D1-AOP-1/src/main/java/cs544/EmailLoggingAspect.java
+++ W1D1-AOP-1/src/main/java/cs544/EmailLoggingAspect.java
@@ -10,8 +10,19 @@ import java.util.Date;
 @Component
 public class EmailLoggingAspect {
 
-    @After("execution(*cs544.EmailSender.sendEmail(..))")
+    // Task 3 — Part (a): After Advice for Email Logging
+    @After("execution(* cs544.EmailSender.sendEmail(..))")
     public void logAfterEmailSent() {
         System.out.println(new Date() + " method= sendEmail");
+    }
+
+    // Task 4 — Part (b): Log Method Arguments
+    @After("execution(* cs544.EmailSender.sendEmail(..)) && args(email, message)")
+    public void logAfterEmailSent(String email, String message) {
+        System.out.println(
+                new Date() +
+                        " method= sendEmail address=" + email +
+                        " message= " + message
+        );
     }
 }
