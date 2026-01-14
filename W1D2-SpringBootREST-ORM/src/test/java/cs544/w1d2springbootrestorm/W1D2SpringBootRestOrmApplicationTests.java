9f4649e6b836d486ee9085ae7b3384cfb64ebbca
Helena Pedro <mbeua94@gmail.com>
2026-01-14 17:59:57 -0600

    new commit

--- W1D2-SpringBootREST-ORM/src/test/java/cs544/w1d2springbootrestorm/W1D2SpringBootRestOrmApplicationTests.java
+++ W1D2-SpringBootREST-ORM/src/test/java/cs544/w1d2springbootrestorm/W1D2SpringBootRestOrmApplicationTests.java
@@ -2,9 +2,8 @@ package cs544.w1d2springbootrestorm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import w1d2springbootrestorm.W1D2SpringBootRestOrmApplication;
 
-@SpringBootTest(classes = W1D2SpringBootRestOrmApplication.class)
+@SpringBootTest
 class W1D2SpringBootRestOrmApplicationTests {
 
 	@Test
