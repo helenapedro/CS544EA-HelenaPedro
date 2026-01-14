2d4aae085dca167666ddb850cce1175ce0cfdb11
Helena Pedro <mbeua94@gmail.com>
2026-01-14 11:00:18 -0600

    Complete Task 5 — Part (c): Access Target Object (JoinPoint)

--- W1D1-AOP-1/src/main/java/cs544/EmailLoggingAspect.java
+++ W1D1-AOP-1/src/main/java/cs544/EmailLoggingAspect.java
@@ -1,5 +1,6 @@
 package cs544;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -11,18 +12,35 @@ import java.util.Date;
 public class EmailLoggingAspect {
 
     // Task 3 — Part (a): After Advice for Email Logging
-    @After("execution(* cs544.EmailSender.sendEmail(..))")
-    public void logAfterEmailSent() {
-        System.out.println(new Date() + " method= sendEmail");
-    }
+//    @After("execution(* cs544.EmailSender.sendEmail(..))")
+//    public void logAfterEmailSent() {
+//        System.out.println(new Date() + " method= sendEmail");
+//    }
 
     // Task 4 — Part (b): Log Method Arguments
+//    @After("execution(* cs544.EmailSender.sendEmail(..)) && args(email, message)")
+//    public void logAfterEmailSent(String email, String message) {
+//        System.out.println(
+//                new Date() +
+//                        " method= sendEmail address=" + email +
+//                        " message= " + message
+//        );
+//    }
+
+    // Task 5 — Part (c): Access Target Object (JoinPoint)
     @After("execution(* cs544.EmailSender.sendEmail(..)) && args(email, message)")
-    public void logAfterEmailSent(String email, String message) {
+    public void logAfterEmailSent(
+            JoinPoint jp,
+            String email,
+            String message) {
+
+        EmailSender sender = (EmailSender) jp.getTarget();
+
         System.out.println(
                 new Date() +
                         " method= sendEmail address=" + email +
-                        " message= " + message
+                        " message= " + message +
+                        " outgoing mail server = " + sender.getOutgoingMailServer()
         );
     }
 }
