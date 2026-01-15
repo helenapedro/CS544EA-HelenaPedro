8480d0d3b4637c0078b9dd8962b01bc35e7a525f
Helena Pedro <mbeua94@gmail.com>
2026-01-14 21:45:39 -0600

    complete project and db setup

---
+++ Optmistic-Locking/src/main/java/cs544/optmisticlocking/OptmisticLockingApplication.java
@@ -0,0 +1,13 @@
+package cs544.optmisticlocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptmisticLockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptmisticLockingApplication.class, args);
+    }
+
+}
