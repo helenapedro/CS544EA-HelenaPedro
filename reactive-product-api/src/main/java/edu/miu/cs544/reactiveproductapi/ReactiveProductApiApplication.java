3f1300176f61e557037c9d08ca48eff503dcd48d
Helena Pedro <mbeua94@gmail.com>
2026-01-16 14:43:59 -0600

    Project setup

---
+++ reactive-product-api/src/main/java/edu/miu/cs544/reactiveproductapi/ReactiveProductApiApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.reactiveproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveProductApiApplication.class, args);
+	}
+
+}
