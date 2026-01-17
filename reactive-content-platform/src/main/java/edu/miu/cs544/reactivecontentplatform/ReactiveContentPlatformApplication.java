b923a3e53ff5aa1d4ad86033b111b13820d89a65
Helena Pedro <mbeua94@gmail.com>
2026-01-17 11:24:00 -0600

    finish project setup

---
+++ reactive-content-platform/src/main/java/edu/miu/cs544/reactivecontentplatform/ReactiveContentPlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.reactivecontentplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveContentPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveContentPlatformApplication.class, args);
+    }
+
+}
---
+++ reactive-content-platform/src/test/java/edu/miu/cs544/reactivecontentplatform/ReactiveContentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.reactivecontentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveContentPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
